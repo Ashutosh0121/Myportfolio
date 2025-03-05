225982f3e927097cdf44e503d2bdc0d4fd8cff87
Ashutosh chauhan <chauhanashutosh287@gmail.com>
2025-03-05 22:55:34 +0530

    '12'

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDom, { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter} from "react-router-dom";
-import Nav from "./Nav";
 import "./main.css"
 
 createRoot(document.getElementById("root")).render(
