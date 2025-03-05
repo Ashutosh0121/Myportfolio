7a16698218ca4d28fffa84253e1ee603c1f9cadf
Ashutosh chauhan <chauhanashutosh287@gmail.com>
2025-03-05 19:17:56 +0530

    4

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDom, { createRoot } from "react-dom/client"
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import Nav from "./Nav"
+import React from "react";
+import ReactDom, { createRoot } from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import Nav from "./Nav";
 
+createRoot(document.getElementById("root")).render(<App />);
 
-createRoot(document.getElementById("root")).render(
-<BrowserRouter>
+{
+  /* <BrowserRouter>
 <App/>
-</BrowserRouter>
-)
+</BrowserRouter> */
+}
