1685cbb32eb6e203ed776a416dee770fa1a364e7
Ashutosh chauhan <chauhanashutosh287@gmail.com>
2025-03-25 22:55:22 +0530

    b

--- src/nav/Resume.jsx
+++ src/nav/Resume.jsx
@@ -4,20 +4,7 @@ import img from "./image.png";
 export default function Resume() {
   return (
     <section>
-      <div className="content">
-
-      <div className="skils">
-        <div>
-          HTML:
-          <div className="box">
-            <div id="html"> </div>
-          </div>
-        </div>
-      </div>
-      <div className="area">
-        <img src={img} alt="" />
-      </div>
-      </div>
+   <h1>hi</h1>
     </section>
   );
 }
