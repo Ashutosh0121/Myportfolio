20e770556dd63011dee387c002690ae373b30a06
Ashutosh chauhan <chauhanashutosh287@gmail.com>
2025-04-02 18:29:49 +0530

    Fix routing issue

--- src/nav/Resume.jsx
+++ src/nav/Resume.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import "./resume.css";
-import img from "./image.png";
+
 export default function Resume() {
   return (
     <section>
-   <h1>hi</h1>
+      <div className="resume-grid">
+        <div className="box bone grid">
+          <img src="public\image\passport photo.jpg" alt="" />
+        </div>
+        <div className="box btwo grid">
+          <p>Ashutosh</p>
+          <p> Chauhan</p>
+        </div>
+        <div className="box bthree grid">
+          
+        </div>
+        <div className="box bfour"></div>
+        <div className="box bfive">a</div>
+        <div className="box blue">s</div>
+        <div className="box green">a</div>
+        <div className="box ">s</div>
+      </div>
     </section>
   );
 }
